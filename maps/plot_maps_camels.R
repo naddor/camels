72f7cbea10c72181c9ff77aa00b65c986b4ea60b
Nans Addor <n.addor@exeter.ac.uk>
2020-01-24 17:24:46 +0000

    improved integraton of layout and tidied up the code

--- maps/plot_maps_camels.R
+++ maps/plot_maps_camels.R
@@ -1,12 +1,16 @@
 plot_map_catch_attr<-function(dat,c2p,n_classes=6,col_scheme='RdYlBu',col_rev=FALSE,color_bar=TRUE,subplot_hist=TRUE,
                               col_trans=0,b_round=2,text_legend='',cex=1,pch=16,qual=FALSE,
-                              force_zero_center=FALSE,force_n_classes=FALSE,set_breaks=FALSE,breaks=NA){
+                              force_zero_center=FALSE,force_n_classes=FALSE,set_breaks=FALSE,breaks=NA,
+                              layout_on=FALSE,layout_ncol=3,layout_nrow=1){
 
   # arguments:
   # dat: data as data.frame
   # c2p: columns to use for plotting
 
+  # check that first column is gauge_id
   if(colnames(dat)[1]!='gauge_id'){stop('First column of dat must be gauge_id')}
+
+  # check that key attributes have either 1 or length(cp) element
   if(!length(col_scheme)%in%c(1,length(c2p))){stop('col_scheme must have either 1 or length(c2p) elements')}
   if(!length(b_round)%in%c(1,length(c2p))){stop('b_round must have either 1 or length(c2p) elements')}
   if(!length(col_rev)%in%c(1,length(c2p))){stop('col_rev must have either 1 or length(c2p) elements')}
@@ -15,8 +19,26 @@ plot_map_catch_attr<-function(dat,c2p,n_classes=6,col_scheme='RdYlBu',col_rev=FA
   if(!length(set_breaks)%in%c(1,length(c2p))){stop('set_breaks must have either 1 or length(c2p) elements')}
   if(!length(breaks)%in%c(1,length(c2p))){stop('breaks must have either 1 or length(c2p) elements')}
 
+  # divide screen to create multiple panels
+  def.par <- par(no.readonly = TRUE) # save default, for resetting...
+
+  if(layout_on){
+
+    n_panels<-layout_ncol*layout_nrow
+
+    if(color_bar){
+      nf<-layout(matrix(1:(n_panels*2),layout_nrow*2,layout_ncol),heights=rep(c(1,0.22),times=layout_nrow),widths=1)
+      # layout.show(nf)
+    }else{
+      nf<-layout(matrix(1:n_panels,layout_nrow,layout_ncol),heights=1,widths=1)
+      #layout.show(nf)
+    }
+  }
+
+  # loop through colums and plot corresponding maps
   for (v in c2p){
 
+    # set plotting details
     my_col_scheme<-ifelse(length(col_scheme)==length(c2p),col_scheme[v==c2p],col_scheme)
     my_b_round<-ifelse(length(b_round)==length(c2p),b_round[v==c2p],b_round)
     my_col_rev<-ifelse(length(col_rev)==length(c2p),col_rev[v==c2p],col_rev)
@@ -26,29 +48,38 @@ plot_map_catch_attr<-function(dat,c2p,n_classes=6,col_scheme='RdYlBu',col_rev=FA
     my_force_n_classes<-ifelse(length(force_n_classes)==length(c2p),force_n_classes[v==c2p],force_n_classes)
     my_set_breaks<-ifelse(length(set_breaks)==length(c2p),set_breaks[v==c2p],set_breaks)
 
-    if(length(breaks)==length(c2p)){ # if loop is necessary because ifelse only returns one element
+    # set breaks
+    if(length(breaks)==length(c2p)){ #
 
       if(length(which(v==c2p))>1){
         stop('The same column is used twice')
       }
 
       my_breaks<-breaks[[which(v==c2p)]]
+
     }else{
+
       my_breaks<-breaks
+
     }
 
     plot_points_us_basins(dat[,c(1,v)],n_classes,col_scheme=my_col_scheme,col_rev=my_col_rev,color_bar,
                           subplot_hist=my_subplot_hist,col_trans,b_round=my_b_round,text_legend=colnames(dat)[v],
                           cex,pch,qual=my_qual,force_zero_center=my_force_zero_center,force_n_classes=my_force_n_classes,
-                          set_breaks=my_set_breaks,breaks=my_breaks)
+                          set_breaks=my_set_breaks,breaks=my_breaks,layout_on=layout_on)
 
   }
+
+  par(def.par)  #- reset to default
+
 }
 
 plot_points_us_basins<-function(dat,n_classes=6,col_scheme='RdYlBu',col_rev=FALSE,color_bar=TRUE,
                                 subplot_hist=TRUE,col_trans=0,b_round=2,text_legend='',cex=1,pch=16,qual=FALSE,
                                 force_zero_center=FALSE,force_n_classes=FALSE,
-                                set_breaks=FALSE,breaks=NA){
+                                set_breaks=FALSE,breaks=NA,layout_on=FALSE){
+
+  # purpose: merge coordinates with variables to plot based on camels_topo
 
   # input variables:
   # dat: matrix with two columns: catchment id (must be named 'gauge_id') and variable to plot
@@ -60,6 +91,7 @@ plot_points_us_basins<-function(dat,n_classes=6,col_scheme='RdYlBu',col_rev=FALS
   # text_legend: text to add above the color bar
   # qual: is the information qualitative (e.g. vegetation classes)
 
+  # load coordinates
   if(!exists('camels_topo')){
 
     load(paste(dir_catch_attr,'camels_topo.Rdata',sep=''))
@@ -71,7 +103,7 @@ plot_points_us_basins<-function(dat,n_classes=6,col_scheme='RdYlBu',col_rev=FALS
   if(dim(dat2plot)[1]==0){stop('Merge with topo failed, check gauge_id')}
 
   plot_points(x=dat2plot$gauge_lon,y=dat2plot$gauge_lat,z=dat2plot[,2],n_classes,col_scheme,col_rev,color_bar,subplot_hist,
-                 col_trans,b_round,text_legend,cex,pch,qual,force_zero_center,force_n_classes,set_breaks,breaks)
+                 col_trans,b_round,text_legend,cex,pch,qual,force_zero_center,force_n_classes,set_breaks,breaks,layout_on=layout_on)
 
 }
 
@@ -80,9 +112,9 @@ plot_points_us_basins<-function(dat,n_classes=6,col_scheme='RdYlBu',col_rev=FALS
 plot_points<-function(x,y,z,n_classes=6,col_scheme='RdYlBu',col_rev=FALSE,color_bar=TRUE,subplot_hist=TRUE,
                          col_trans=0,b_round=2,text_legend='',cex=1,pch=16,qual=FALSE,
                          force_zero_center=FALSE,force_n_classes=FALSE,set_breaks=FALSE,breaks=NA,
-                         country='us'){
+                         country='us',layout_on=FALSE){
 
-  # works well for pdf width=10,height=7
+  # purpose: plot map for chosen country and add a dot per catchment
 
   # input variables:
   # x,y: coordinates
@@ -102,16 +134,22 @@ plot_points<-function(x,y,z,n_classes=6,col_scheme='RdYlBu',col_rev=FALSE,color_
 
   if(force_zero_center&n_classes%%2!=0){stop('n_classes must be an even number if force_zero_center is TRUE')}
 
-  # plot background map
+  # setup layout - this might have to be changed for when layout_on=FALSE
   if(color_bar){
-    layout(matrix(1:2,2,1),heights=c(4,0.7),widths=1)
+    if(!layout_on){
+      layout(matrix(1:2,2,1),heights=c(4,0.7),widths=1)
+    }
+  } else{
+    par(mfrow=c(1,1))
   }
 
-  par(mar=c(0,0,0,0))
+  # initialise par
+  par(mar=c(0,0,0,0),cex=1)
 
+  # plot background map
   if(country=='us'){
 
-    map("state",col='gray60',fill=TRUE,border=NA)
+    map("state",col='gray60',fill=TRUE,border=NA,xlim=c(-125,-67),ylim=c(25,50))
     map("state",col='gray89',add=TRUE,lwd=1,resolution=0)
     map("state",col='black',add=TRUE,lwd=0.8,resolution=0,interior = FALSE)
 
@@ -139,6 +177,9 @@ plot_points<-function(x,y,z,n_classes=6,col_scheme='RdYlBu',col_rev=FALSE,color_
 
       b<-breaks
 
+      # show breaks
+      message(paste0('set_breaks=TRUE, using these values'))
+
     }else{
 
       b<-unique(round(quantile(z,seq(1/n_classes,1-1/n_classes,length.out=n_classes-1),na.rm = TRUE),b_round))
@@ -157,11 +198,14 @@ plot_points<-function(x,y,z,n_classes=6,col_scheme='RdYlBu',col_rev=FALSE,color_
         b[n_classes/2]<-0
       }
 
+      # show breaks
+      message(paste0('set_breaks=FALSE, using these values for breaks'))
+
     }
 
-    # show breaks
     print(b)
 
+    # define colors
     if(length(b)<2){ # the mimimum number of color delivered by colorbrewer is 3
 
       col<-brewer.pal(4,col_scheme)[1:(length(b)+1)]
@@ -284,6 +328,8 @@ plot_points<-function(x,y,z,n_classes=6,col_scheme='RdYlBu',col_rev=FALSE,color_
   # add historgram if required
   if(subplot_hist){
 
+    message('Trying to plot histogram')
+
     if(qual){
 
       par(las=3,cex=0.8)
@@ -308,7 +354,7 @@ plot_points<-function(x,y,z,n_classes=6,col_scheme='RdYlBu',col_rev=FALSE,color_
       plot.legend.na(col, b, vert=FALSE)
     }
   }else{
-    par(mar=c(0,0,0,0),cex=1) # if necessary, reduce cex to fit more information
+    par(mar=c(3,5,0,5),cex=1)
     plot.new()
     if(pch>=21){
      legend('top',pt.bg=as.character(col_table$R_color),legend=col_table$categ,pch=pch,ncol=2,bty='n')
@@ -317,9 +363,8 @@ plot_points<-function(x,y,z,n_classes=6,col_scheme='RdYlBu',col_rev=FALSE,color_
      }
    }
 
-  # reset par and layout
-  par(mar=c(0,0,0,0),cex=1)
-  layout(1,heights=1,widths=1) # comment to use layout in another part of the script
+  # reset par
+  # par(mar=c(0,0,0,0),cex=1)
 
 }
 
