5f85505e68e221a2eb6b66d175c5f87d074a6bdd
uschoene <88304501+uschoene@users.noreply.github.com>
2023-03-27 10:17:37 +0200

    corrected wrongly set column name

--- landcover_attributes/camels_ch_annual_timeserie.R
+++ landcover_attributes/camels_ch_annual_timeserie.R
@@ -7,11 +7,10 @@
 # ==============================================================================
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-# make sure to have input files in the same folder as script
 
 ##################################
 ## prepare lists and variables
-list_files_clc<-c("crop_perc", "ewood_perc", "dwood_perc", "grass_perc", "ice_perc","inwater_perc", "loose_rock_perc", "mix_wood_perc", "rock_perc", "shrub_perc", "urban_perc", "wetlands_perc","blank_perc")
+list_files_clc<-c("crop_perc", "dwood_perc", "ewood_perc", "grass_perc", "ice_perc","inwater_perc", "loose_rock_perc", "mix_wood_perc", "rock_perc", "shrub_perc", "urban_perc", "wetlands_perc","blank_perc")
 list_files_glacier<-c("glac_area_ch_[km2]","glac_vol_ch_[km3]","glac_mass_ch_[Mt]","glac_area_neighbours_[km2]")
 list_parameter_name<-c(list_files_clc,list_files_glacier)
 
@@ -169,12 +168,14 @@ for (k in 1:length(catch_names)){
     param_daten <- data.frame(data_list_all[j])
     data_catchment[,j] <- param_daten[,k]
   }
-  # add column names (attributes)
+  # change order of columns and add column names (attributes)
   colnames(data_catchment) <- list_parameter_name
+  data_catchment_2<-cbind(rownames(data_catchment),data_catchment[,c(1,4,10,2,8,3,12,6,5,7,9,11,13,14,15,16,17)])
+  colnames(data_catchment_2)<-c("year",colnames(data_catchment_2)[2:17])
   # save as file
   file_name<-(paste0("CAMELS_CH_annual_data_",catch_ID,".csv"))
   first_row_text<-paste0("gauge_id=",catch_ID,", Land cover attributes as area percentages based on the CORINE land cover databases by the European Copernicus Land Monitoring Service: 1990, 2000, 2006, 2012 and 2018. The years inbetween were interpolated by linear regression. In 1990 only areas outside Switzerland were covered by clc_1990, therefore only catchments with more then 95% data in clc_1990 were calculated. glaciers source: Swiss Glacier Inventory 1973/2016 and Paul et al, 2020. glacier_mass = glacier_volume*850.")
   cat(first_row_text,"\n", file=file_name)
-  write.table(data_catchment, file = file_name, append=TRUE, quote=FALSE,sep=";",fileEncoding="UTF-8")
+  write.table(data_catchment_2, file = file_name, append=TRUE, row.names=FALSE, quote=FALSE,sep=";",fileEncoding="UTF-8")
 }
 
